ae42bdf0bf6acdd9927f7e81a347f0ca63802e68
Richard <richrd77@gmail.com>
2024-08-17 05:34:59 +0530

    responsive header

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  @ViewChild('nav') private nav!: ElementRef<HTMLElement>;
+
+  public ShowHeaderMenu(): void {
+    this.nav.nativeElement.classList.add('show');
+  }
+
+  @HostListener('document:click', ['$event'])
+  private HideHeaderMenu(event: MouseEvent): void {
+    const clickedElement = (event.target as HTMLElement);
+    const hamButton = this.closestParent(clickedElement, 'button.ham');
+
+    if (!hamButton) {
+      this.nav.nativeElement.classList.remove('show');
+    }
+  }
+
+  private closestParent(element: HTMLElement, selector: string): HTMLElement | null {
+    let parent: HTMLElement | null = element.parentElement;
+
+    while (parent) {
+      if (parent.matches(selector)) {
+        return parent;
+      }
+      parent = parent.parentElement;
+    }
+
+    return null;
+  }
 }
