3abda444e15617c324b8ab10191a7aadc8ec4a9f
Richard <richrd77@gmail.com>
2024-08-19 01:54:25 +0530

    scoll to spefici box

--- src/app/common/components/brochure/brochure.component.ts
+++ src/app/common/components/brochure/brochure.component.ts
@@ -1,18 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 import { Box } from '../../models';
 import { BaseLocationService } from '../../services/base-location.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-brochure',
   templateUrl: './brochure.component.html',
   styleUrl: './brochure.component.scss'
 })
-export class BrochureComponent {
+export class BrochureComponent implements AfterViewInit {
+
+  private focusedDiv: string | undefined;
 
   @Input({ required: true }) public title!: string;
 
   @Input({ required: true }) public boxes!: Box[];
 
-  constructor(public base: BaseLocationService) { }
+  constructor(public base: BaseLocationService, private router: Router) {
+    const st = router.getCurrentNavigation()?.extras?.state;
+    this.focusedDiv = st ? st['routeId'] : undefined;
+  }
+
+  ngAfterViewInit(): void {
+    const foundDiv = document.querySelector(`#${this.focusedDiv}`) as HTMLDivElement | undefined;
+
+    if (this.focusedDiv && foundDiv) {
+      setTimeout(() => {
+        foundDiv.scrollIntoView({ behavior: 'smooth' });
+      }, 0.3 * (this.boxes.length - 1));
+    }
+  }
 
 }
