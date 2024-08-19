069c9db824ad3538a02ac6915cfe4ad30cd8c1cc
Richard <richrd77@gmail.com>
2024-08-19 11:24:06 +0530

    industrty product

--- src/app/automation/components/automation-products/automation-products.component.ts
+++ src/app/automation/components/automation-products/automation-products.component.ts
@@ -16,6 +16,16 @@ export class AutomationProductsComponent {
     { title: 'Lights & CCTV automation', img: 'images/automation/office/parking.gif', alt: 'parking', desc: 'office parking automation.', targetRoute: '/automation/office', param: 'four' },
   ];
 
+  public industryProducts: any[] = [
+    { title: 'Fleet management system on cloud', img: 'images/automation/industry/fms.png', alt: 'fleetmanagement', desc: 'fleet managment automation.', targetRoute: '/automation/industry', param: 'one' },
+    { title: 'Machine Automation', img: 'images/automation/industry/machine.png', alt: 'machine', desc: 'automate your electrical machines.', targetRoute: '/automation/industry', param: 'two' },
+    { title: 'Cold storage truck Automation', img: 'images/automation/industry/coldstorage.png', alt: 'coldstorage', desc: 'cold storage truck monitoring.', targetRoute: '/automation/industry', param: 'three' },
+    { title: 'Asset tracking', img: 'images/automation/industry/tracking.png', alt: 'tracking', desc: 'asset tracking.', targetRoute: '/automation/industry', param: 'four' },
+    { title: 'SoS panic button automation', img: 'images/automation/industry/sos.png', alt: 'sos', desc: 'Panic button automation.', targetRoute: '/automation/industry', param: 'five' },
+    { title: 'Power station automation', img: 'images/automation/industry/substation.png', alt: 'substation', desc: 'Automation the power station monitoring/operation.', targetRoute: '/automation/industry', param: 'six' },
+    { title: 'Solar farm automation', img: 'images/automation/industry/solar.png', alt: 'solar', desc: 'monitor solar farm remotely.', targetRoute: '/automation/industry', param: 'seven' },
+  ];
+
   constructor(public path: BaseLocationService, private router: Router) { }
 
   public Navigate(route: string, param: string): void {
