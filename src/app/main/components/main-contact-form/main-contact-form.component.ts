553c1bdb410ef6f2f829d6c2c969b0c1e75f8f80
Richard <richrd77@gmail.com>
2024-09-17 17:48:21 +0530

    dynamic contact v2

--- src/app/main/components/main-contact-form/main-contact-form.component.ts
+++ src/app/main/components/main-contact-form/main-contact-form.component.ts
@@ -10,15 +10,100 @@ import { MatCheckboxChange } from '@angular/material/checkbox';
 export class MainContactFormComponent {
   public formGroup: FormGroup;
   private categories: number[];
+  private isSubmitted = false;
 
   constructor() {
     this.categories = [];
     this.formGroup = new FormGroup({
-      phone: new FormControl<string>('', [Validators.required]),
+      phone: new FormControl<string>('', [Validators.required, Validators.pattern('^\\d{10}$')]),
       email: new FormControl<string>('', [Validators.required, Validators.email]),
       category: new FormControl<number[]>([], [Validators.required]),
       query: new FormControl<string>('', [Validators.required]),
     });
+  }
+
+  public get ValidQuery(): string {
+    const crontol: AbstractControl<string> | null = this.formGroup.get('query');
+    
+    if (!crontol) {
+      return '';
+    }
+
+    if (!crontol.errors) {
+      return '';
+    }
+
+    if (!this.isSubmitted && !this.formGroup.touched
+      && !this.formGroup.dirty) {
+      return '';
+    }
+
+    const errors: string[] = Object.keys(crontol.errors);
+
+    return errors[0];
+  }
+
+  public get ValidCategory(): string {
+    const crontol: AbstractControl<number[]> | null = this.formGroup.get('category');
+    
+    if (!crontol) {
+      return '';
+    }
+
+    if (!crontol.errors) {
+      return '';
+    }
+
+    if (!this.isSubmitted && !this.formGroup.touched
+      && !this.formGroup.dirty) {
+      return '';
+    }
+
+    const errors: string[] = Object.keys(crontol.errors);
+
+    return errors[0];
+  }
+
+  public get ValidPhone(): string {
+    const crontol: AbstractControl<string> | null = this.formGroup.get('phone');
+    
+    if (!crontol) {
+      return '';
+    }
+
+    if (!crontol.errors) {
+      return '';
+    }
+
+    if (!this.isSubmitted && !this.formGroup.touched
+      && !this.formGroup.dirty) {
+      return '';
+    }
+
+    const errors: string[] = Object.keys(crontol.errors);
+
+    return errors[0];
+  }
+
+  public get ValidEmail(): string {
+    const crontol: AbstractControl<string> | null = this.formGroup.get('email');
+    
+    if (!crontol) {
+      return '';
+    }
+
+    if (!crontol.errors) {
+      return '';
+    }
+
+    if (!this.isSubmitted && !this.formGroup.touched
+      && !this.formGroup.dirty) {
+      return '';
+    }
+
+    const errors: string[] = Object.keys(crontol.errors);
+
+    return errors[0];
   }
 
   public CategoryChangeEvent(event: MatCheckboxChange): void {
@@ -42,6 +127,7 @@ export class MainContactFormComponent {
   }
 
   public SubmitQuery(): void {
+    this.isSubmitted = true;
     console.log('submit', this.formGroup.getRawValue());
   }
 }
