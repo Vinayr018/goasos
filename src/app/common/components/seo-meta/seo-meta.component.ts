1d19b2d54ec2357e74ff21ad1c0613556d64fdc8
Richard <richrd77@gmail.com>
2024-11-17 22:16:52 +0530

    changed approach

--- src/app/common/components/seo-meta/seo-meta.component.ts
+++ src/app/common/components/seo-meta/seo-meta.component.ts
@@ -1,36 +1,53 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-seo-meta',
-  template: `<script type="application/ld+json">
-  {
-    "@context": "http://schema.org",
-    "@type": "product",
-    "headline": "Products : Goasos",
-    "author": "goasos dev team",
-    "description": "Home and office automation in bangalore, bhubaneswar, cuttack and USA",
-    "url": "https://goasos.com/product",
-    "name":"Home and Office Automation products",
-    review": {
-      "@type": "Review",
-      "reviewRating": {
-        "@type": "Rating",
-        "ratingValue": 4,
-        "bestRating": 5
-      },
-      "author": {
-        "@type": "Team",
-        "name": "Goasos dev team"
-      }
-    },
-    "aggregateRating": {
-      "@type": "AggregateRating",
-      "ratingValue": 4.4,
-      "reviewCount": 89
-    }
-  }
-</script>`,
+  template: ``,
 })
-export class SeoMetaComponent {
+export class SeoMetaComponent implements OnInit, OnDestroy {
 
+  private script: any = null;
+
+  constructor(@Inject(DOCUMENT) private doc: Document,
+    private render: Renderer2) { }
+
+  ngOnDestroy(): void {
+    this.render.removeChild(this.doc.head, this.script);
+  }
+
+  ngOnInit(): void {
+    this.script = this.render.createElement('script');
+    this.script.type = `application/ld+json`;
+    this.script.text = `
+    {
+      "@context": "http://schema.org",
+      "@type": "product",
+      "headline": "Products : Goasos",
+      "author": "goasos dev team",
+      "description": "Home and office automation in bangalore, bhubaneswar, cuttack and USA",
+      "url": "https://goasos.com/product",
+      "name":"Home and Office Automation products",
+      "review": {
+        "@type": "Review",
+        "reviewRating": {
+          "@type": "Rating",
+          "ratingValue": 4,
+          "bestRating": 5
+        },
+        "author": {
+          "@type": "Team",
+          "name": "Goasos dev team"
+        }
+      },
+      "aggregateRating": {
+        "@type": "AggregateRating",
+        "ratingValue": 4.4,
+        "reviewCount": 89
+      }
+    }
+    `;
+
+    this.render.appendChild(this.doc.head, this.script);
+  }
 }
