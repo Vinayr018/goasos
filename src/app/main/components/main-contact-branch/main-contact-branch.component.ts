880a8f3e26febbb518828c732ba496607ce0db47
Richard <richrd77@gmail.com>
2025-05-25 21:31:02 +0530

    contact v1

--- src/app/main/components/main-contact-branch/main-contact-branch.component.ts
+++ src/app/main/components/main-contact-branch/main-contact-branch.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+import { AnalyticsService } from '../../../common/services/analytics.service';
 
 @Component({
   selector: 'app-main-contact-branch',
@@ -7,17 +7,5 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrl: './main-contact-branch.component.scss'
 })
 export class MainContactBranchComponent {
-  @Input({ required: true }) public heading!: string;
-  @Input({ required: true }) public numbers!: string[];
-  @Input({ required: true }) public location!: string;
-  @Input() public isEmail: boolean = false;
-
-  constructor(public domSanitizer: DomSanitizer) { }
-
-  public sanitizePhoneNumber(number: string): string {
-    let app = this.isEmail ? 'mailto:' : 'tel:';
-    let cleanUrl = number.replace(/\s+/g, '');
-    cleanUrl = this.isEmail ? cleanUrl : cleanUrl.replace(/-/g, '')
-    return `${app}${cleanUrl}`;
-  }
+  constructor(public analytics: AnalyticsService) { }
 }
